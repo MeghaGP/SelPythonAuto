00eebb67bbbd81a1fd751c82da9c4aa0f168b675
MeghaGP <meghagp.rymec@gmail.com>
2018-11-15 09:38:06 -1200

    first commit

---
+++ duplicate characters.py
@@ -0,0 +1,35 @@
+str1 = input("enter the string: ")
+repeated_letter={i:str1.count(i) for i in (str1) if str1.count(i)>1}
+print(repeated_letter)
+
+#def counter(strr):
+ #  for i in strr:
+  #      if i in dic:
+   #         dic[i]+=1
+    #    else:
+     #       dic[i]=1
+    #return dic
+#print(counter("bharath"))
+
+#a="bharath"
+#b=list(a)
+#y=set(b)
+#x={}
+#print(b)
+#print(y)
+#for i in y:
+ #   x[i]=b.count(i)
+
+  #  keys=x.items()
+#for k,v in keys:
+ #   if v>=2 and k!=" ":
+  #      print(k,v)
+
+
+#def x(str1):
+ #   for i in "abcdefghijklmnopqrstuvwxy":
+  #      y=str1.count(i)
+   #     if y>1:
+    #        print(i+" "+str(y))
+
+#x("bharath")
